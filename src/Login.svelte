58cc9f397ec39710b262e232f5e5ea02b58db271
i.manzhela <i.manzhela@samsung.com>
2023-10-09 11:45:58 +0200

    loggining form created

--- src/Login.svelte
+++ src/Login.svelte
@@ -1,15 +1,35 @@
 <script>
+  import InputRectangle from "./components/InputRectangle/InputRectangle.svelte";
 </script>
 
 <div>
+  <form class="login-form">
+    <InputRectangle name="Login:"/>
+    <InputRectangle type="password" name="Password:"/>
+    <button>Login</button>
+  </form>
 </div>
 
 <style>
 div {
-  border: 1px black solid;
   height: 100px;
-  width: 100px;
+  width: 33vw;
   margin: auto;
-  margin-top: 2rem;
+  margin-top: 25vh;
 }
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+}
+
+button {
+  height: 3rem;
+  font-size: 1.5rem;
+  width: 30%;
+  align-self: flex-end;
+  border-radius: 4px;
+}
+
 </style>
