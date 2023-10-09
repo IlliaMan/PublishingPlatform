85ac0a1f9098a76c2be273424a985e804ffcc02b
i.manzhela <i.manzhela@samsung.com>
2023-10-09 11:44:29 +0200

    InputRectangle added

---
+++ src/components/InputRectangle/InputRectangle.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let name = "Default Name:";
+  export let type = "text";
+</script>
+
+<div>
+    <p>{name}</p>
+    <input type={type} />
+</div>
+
+<style>
+div {
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+}
+
+p { 
+  align-self: flex-start;
+  margin: 0;
+  font-size: 1.5rem;
+  color: white;
+}
+
+input {
+  height: 3rem;
+  border-radius: 0.5rem;
+  font-size: 1.5rem;
+  width: 100%;
+}
+</style>
