70f9a2e684051ada7674b5630f265a63ce078a74
i.manzhela <i.manzhela@samsung.com>
2023-11-22 15:49:13 +0100

    custom router using the footer added

--- src/App.svelte
+++ src/App.svelte
@@ -8,52 +8,68 @@
 	import ArticlePage from './ArticlePage.svelte';
 	import UserPage from './UserPage.svelte';
 	import EditArticlePage from './EditArticlePage.svelte';
-</script>
 
-<!-- <main>
-	<Header />
-	<Login />
-</main> -->
+	let pages = {
+		login: 0,
+		userPage: 1,
+		forgotPage: 2,
+		mainPage: 3,
+		artilePage: 4,
+		subscribePage: 5,
+		signupPage: 6,
+		editArticlePage: 7
+	};
+	
+	let activePage = pages.mainPage;
+</script>
 
 <main>
 	<Header />
-	<EditArticlePage />
+	{#if activePage === pages.mainPage}
+    <MainPage />
+  {:else if activePage === pages.login}
+    <Login />
+	{:else if activePage === pages.userPage}
+    <UserPage />
+	{:else if activePage === pages.forgotPage}
+    <ForgotPage />
+	{:else if activePage === pages.artilePage}
+    <ArticlePage />
+	{:else if activePage === pages.subscribePage}
+    <SubscribePage />
+	{:else if activePage === pages.signupPage}
+    <SignUpPage />
+	{:else if activePage === pages.editArticlePage}
+    <EditArticlePage />
+  {/if}
+
+	<footer>
+		{#each Object.keys(pages) as page}
+			<button on:click={() => {
+				activePage = pages[page];
+			}
+			}>{page}</button>
+		{/each}
+	</footer>
 </main>
-
-<!-- <main>
-	<Header />
-	<SignUpPage />
-</main> -->
-
-<!-- <main>
-	<Header />
-	<SubscribePage />
-</main> -->
-
-<!-- <main>
-	<Header />
-	<MainPage />
-</main> -->
-
-<!-- <main>
-	<Header />
-	<ArticlePage />
-</main> -->
-
-<!-- <main>
-	<Header />
-	<ForgotPage />
-</main> -->
-
-<!-- <main>
-	<Header />
-	<UserPage />
-</main> -->
 
 <style>
 	main {
 		text-align: center;
 		padding: 0;
 		margin: 0;
+	}
+
+	footer {
+		position: fixed;
+		width: 100%;
+		min-height: 3rem;
+		background-color: #ddd;
+		bottom: 0rem;
+	}
+	
+	button {
+		min-height: 3rem;
+		min-width: 5rem;
 	}
 </style>
