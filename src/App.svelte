725d64182acb077bc9f2efeaeee87ceba630ca3c
i.manzhela <i.manzhela@samsung.com>
2023-11-22 10:45:45 +0100

    all the pages added without routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,47 @@
 <script>
 	import Header from './Header.svelte';
+	import ForgotPage from './ForgotPage.svelte';
 	import Login from './Login.svelte';
+	import SignUpPage from './SignUpPage.svelte';
+	import SubscribePage from './SubscribePage.svelte';
+	import MainPage from './MainPage.svelte';
+	import ArticlePage from './ArticlePage.svelte';
+	import UserPage from './UserPage.svelte';
 </script>
+
+<!-- <main>
+	<Header />
+	<Login />
+</main> -->
+
+<!-- <main>
+	<Header />
+	<SignUpPage />
+</main> -->
+
+<!-- <main>
+	<Header />
+	<SubscribePage />
+</main> -->
+
+<!-- <main>
+	<Header />
+	<MainPage />
+</main> -->
+
+<!-- <main>
+	<Header />
+	<ArticlePage />
+</main> -->
+
+<!-- <main>
+	<Header />
+	<ForgotPage />
+</main> -->
 
 <main>
 	<Header />
-	<Login />
+	<UserPage />
 </main>
 
 <style>
