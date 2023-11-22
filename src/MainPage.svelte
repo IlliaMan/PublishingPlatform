74ac19b13fdea0a5adfe4ceb2f9a81e25c0936db
i.manzhela <i.manzhela@samsung.com>
2023-11-22 09:59:38 +0100

    more dummy articles added in MainPage

--- src/MainPage.svelte
+++ src/MainPage.svelte
@@ -22,6 +22,22 @@
     {
       title: "Article Title 5",
       content: "This description provides a brief overview, giving readers a quick glimpse of its content to help them decide if they are interested"
+    },
+    {
+      title: "Article Title 6",
+      content: "This description provides a brief overview, giving readers a quick glimpse of its content to help them decide if they are interested"
+    },
+    {
+      title: "Article Title 7",
+      content: "This description provides a brief overview, giving readers a quick glimpse of its content to help them decide if they are interested"
+    },
+    {
+      title: "Article Title 8",
+      content: "This description provides a brief overview, giving readers a quick glimpse of its content to help them decide if they are interested"
+    },
+    {
+      title: "Article Title 59",
+      content: "This description provides a brief overview, giving readers a quick glimpse of its content to help them decide if they are interested"
     }
   ]
 </script>
