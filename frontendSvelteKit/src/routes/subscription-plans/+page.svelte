58bcad4f2209c79dafaf5da1b2b5198075b0d6db
i.manzhela <illiamanzhela01@gmail.com>
2023-12-02 23:07:30 +0100

    routes/subscription-plans added

---
+++ frontendSvelteKit/src/routes/subscription-plans/+page.svelte
@@ -0,0 +1,83 @@
+<script>
+</script>
+
+<div class="main">
+  <h1>Get unlimited access</h1>
+  <p>Subscription start at 1$/week</p>
+  <div class="options">
+    <div class="option-1 option">
+      <h1>Weekly</h1>
+      <p>1$ a week</p>
+      <button>Select</button>
+    </div>
+    <div class="option-2 option">
+      <h1>Monthly</h1>
+      <p>4$ a month</p>
+      <button>Select</button>
+    </div>
+    <div class="option-3 option">
+      <h1>Annually</h1>
+      <p>40$ a year</p>
+      <button>Select</button>
+    </div>
+  </div>
+</div>
+
+<style>
+  .main {
+    height: 100px;
+    width: 50vw;
+    margin: auto;
+    margin-top: 18vh;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+
+  h1 {
+    font-size: 4rem;
+    margin: 0;
+  }
+
+  p {
+    font-size: 1.6rem;
+    margin: 0;
+  }
+
+  .options {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    column-gap: 2rem;
+  }
+
+  .options > div {
+    min-width: 30%;
+    min-height: 35rem;
+    background-color: #fff;
+  }
+
+  .option:nth-child(2) {
+    background-color: #aaa;
+  }
+
+  .option {
+    box-sizing: border-box;
+    padding: 4rem 0rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  p {
+    font-size: 2rem;
+  }
+
+  button {
+    min-height: 5rem;
+    font-size: 2rem;
+    width: 60%;
+    align-self: center;
+    border-radius: 8px;
+  }
+</style>
