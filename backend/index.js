1f7ff1fa60929e47c16fe7d9d07e55853cff0e27
i.manzhela <illiamanzhela01@gmail.com>
2023-12-03 21:27:10 +0100

    GET /login added

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/login', (req, res) => {
+  console.log(req.query);
+  res.json({ userName: "UserNumberOne"});
+});
+
 app.listen(port, () => {
   console.log(`listening at port ${port}`);
 });
