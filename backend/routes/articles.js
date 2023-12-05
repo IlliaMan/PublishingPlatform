6189fc8d63a0898da336457d5c230fd1fa0a7c4c
i.manzhela <illiamanzhela01@gmail.com>
2023-12-05 16:18:56 +0100

    CRUD added for articles

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import ArticleModel from '../models/article.js';
 
 const articlesRouter = express.Router();
@@ -6,11 +6,77 @@ const articlesRouter = express.Router();
 // Getting all
 articlesRouter.get('/', async (request, response) => {
   try {
-    const artciles = await ArticleModel.find();
-    response.json(artciles);
+    const articles = await ArticleModel.find();
+    response.json(articles);
   } catch (error) {
     response.status(500).json({ message: error.message });
   }
 });
+
+// Getting one 
+articlesRouter.get('/:id', getArticle, (req, res) => {
+  res.json(res.article);
+});
+
+// Creating one 
+articlesRouter.post('/', async (req, res) => {
+  console.log(req.body);
+  const article = new ArticleModel({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  try {
+    const newArticle = await article.save();
+    // 201 - successfully created an object
+    res.status(201).json(newArticle);
+  } catch (error) {
+    // 400 - something wrong with the user input
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Updating one 
+articlesRouter.patch('/:id', getArticle, async (req, res) => {
+  if(req.body.title !== undefined) {
+    res.article.title = req.body.title;
+  }
+  if(req.body.content !== undefined) {
+    res.article.content = req.body.content;
+  }
+
+  try {
+    const updatedArticle = await res.article.save();
+    res.json(updatedArticle);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Deleting one 
+articlesRouter.delete('/:id', async (req, res) => {
+  try {
+    await res.article.remove();
+    res.json({ message: 'Deleted Article'});
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+async function getArticle(req, res, next) {
+  let article;
+  try {
+    article = await ArticleModel.findById(req.params.id);
+    if(article == null) {
+      // 404 - cannot find something
+      return res.status(404).json({ message: "cannot find Article"});
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  res.article = article;
+  next();
+}
 
 export default articlesRouter;
