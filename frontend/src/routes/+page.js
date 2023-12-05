1da5534741f6ac972ca58595bddb88ad36f4efb8
i.manzhela <illiamanzhela01@gmail.com>
2023-12-05 18:01:32 +0100

    the endpoint for .article-tiles changed

--- frontend/src/routes/+page.js
+++ frontend/src/routes/+page.js
@@ -1,5 +1,5 @@
 export async function load({ fetch, params }) {
-  const response = await fetch('http://localhost:3000/article-list');
+  const response = await fetch('http://localhost:3000/articles/');
   return await  {
     articleList: response.json()
   };
