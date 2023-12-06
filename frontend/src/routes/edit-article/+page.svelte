efbb1eceb13ef202b5053aa35efb15e5ef9e9da5
i.manzhela <illiamanzhela01@gmail.com>
2023-12-06 11:14:15 +0100

    saving edited article gets saved in the db

--- frontend/src/routes/edit-article/+page.svelte
+++ frontend/src/routes/edit-article/+page.svelte
@@ -2,16 +2,46 @@
   import InputRectangle from "$lib/components/InputRectangle.svelte";
   import showdown from 'showdown';
 
-  let text = `# Markdown syntax guide\n\n## Headers\n\n# This is a Heading h1\n## This is a Heading h2\n###### This is a Heading h6\n\n## Emphasis\n\n*This text will be italic*  \n_This will also be italic_\n\n**This text will be bold**\n__This will also be bold__\n\ngitg_You **can** combine them_`
+  let text = `# Markdown syntax guide\n\n## Headers\n\n# This is a Heading h1\n## This is a Heading h2\n###### This is a Heading h6\n\n## Emphasis\n\n*This text will be italic*  \n_This will also be italic_\n\n**This text will be bold**\n__This will also be bold__\n\ngitg_You **can** combine them_`;
+  let articleTitle;
+  let id = window.location.search.substring(1).split('=')[1];
+  async function fetchArticles() {
+    const res = await fetch(`http://127.0.0.1:3000/articles/${id}`);
+    const data = await res.json();
+
+    if (res.ok) {
+      text = data.content;
+      articleTitle = data.title;
+      return data;
+    } else {
+      throw new Error(data);
+    }
+  }
+
   let converter = new showdown.Converter();
 </script>
 
 <div class="main">
+    {#await fetchArticles()}
+      <p>loading</p>
+    {:then article} 
   <div class="information">
     <div class="article-name">
-      <InputRectangle placeholder="Article.md" name="Document Name"/>
+      <InputRectangle placeholder="Article.md" name="Document Name" value={articleTitle}/>
     </div>
-    <button>Save</button>
+    <button on:click={() => {
+      console.log(`PATCH: http://localhost:3000/articles/${article._id}`);
+      fetch(`http://localhost:3000/articles/${article._id}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          title: articleTitle,
+          content: text
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+    }}>Save</button>
     <button>Publish</button>
     <div class="statistics">
       <p>Reading time: 5 minutes</p>
@@ -22,20 +52,21 @@
   <div class="main-area">
     <div class="editing-area">
       <div class="header">
-        <h1>Article.md</h1>
+        <h1>{`${article.title}.md`}</h1>
       </div>
       <textarea bind:value={text}/>
     </div>
     <div class="display-area">
       <div class="header">
-        <h1>Article.md</h1>
+        <h1>{`${article.title}.md`}</h1>
       </div>
       <div class="preview">
         {@html converter.makeHtml(text)}
       </div>
       <!-- <textarea bind:value={md} disabled /> -->
     </div>
-  </div>
+    </div>
+    {/await}
 </div>
 
 <style>
@@ -68,7 +99,6 @@
   h1 {
     margin: 0;
     padding: 1rem;
-    max-width: 5rem;
   }
   
   .header {
