3c646650065840fed94b11a39b352a24e99024f7
i.manzhela <illiamanzhela01@gmail.com>
2023-12-06 12:10:04 +0100

    endpoint for loggining in added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -29,6 +29,24 @@ userRouter.get('/:id', async (req, res) => {
   res.json(user);
 });
 
+// Getting one 
+userRouter.get('/:email/:password', async (req, res) => {
+  const { email, password } = req.params;
+  let user;
+  try {
+    user = await UserModel.find({ email, password });
+    if(user == null || user.length === 0) {
+      // 404 - cannot find something
+      return res.status(404).json({ message: "cannot find User"});
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  res.json(user);
+});
+
+
 // Creating one 
 userRouter.post('/', async (req, res) => {
   console.log(req.body);
