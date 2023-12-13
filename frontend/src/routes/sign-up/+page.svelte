c175cf01cb2ff299952ad8be1620aa2fbc87d972
i.manzhela <illiamanzhela01@gmail.com>
2023-12-13 23:01:33 +0100

    signing up connected to the database

--- frontend/src/routes/sign-up/+page.svelte
+++ frontend/src/routes/sign-up/+page.svelte
@@ -1,15 +1,57 @@
 <script>
-    import InputRectangle from "$lib/components/InputRectangle.svelte";
+  import { goto } from "$app/navigation";
+  import InputRectangle from "$lib/components/InputRectangle.svelte";
+
+  function onSignupSubmit(event) {
+    const formData = new FormData(event.target);
+
+    const data = {};
+    for (let field of formData) {
+      const [key, value] = field;
+      data[key] = value;
+    }
+
+    console.log(data);
+    fetch('http://localhost:3000/users/', 
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: data.email,
+          username: data.username,
+          password: data.password
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }
+    )
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('Sth went wrong');
+      }
+
+      return response.json();
+    })
+    .then(response => { 
+      console.log(response);
+      goto('/login');
+    })
+    .catch(error => {
+      console.warn(error.message);
+    });
+  }
 </script>
 
 <div class="main">
   <h1>Sign up</h1>
   <p>Create a free account</p>
-  <InputRectangle name="Email" placeholder="youremail@gmail.com"/>
-  <InputRectangle name="Username" placeholder="nickname"/>
-  <InputRectangle name="Password" placeholder="**********" type="password"/>
-  <InputRectangle name="Repeat" placeholder="**********" type="password"/>
-  <button>Sign up</button>
+  <form on:submit|preventDefault={onSignupSubmit}>
+    <InputRectangle inputName="email" name="Email" placeholder="youremail@gmail.com"/>
+    <InputRectangle inputName="username" name="Username" placeholder="nickname"/>
+    <InputRectangle inputName="password" name="Password" placeholder="**********" type="password"/>
+    <InputRectangle name="Repeat" placeholder="**********" type="password"/>
+    <button type="submit">Sign up</button>
+  </form>
   <div class="additional-options">
     <a href="/login">
       <p>Back to sign in</p>
@@ -23,6 +65,12 @@
     width: 33vw;
     margin: auto;
     margin-top: 18vh;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+
+  form {
     display: flex;
     flex-direction: column;
     gap: 2rem;
