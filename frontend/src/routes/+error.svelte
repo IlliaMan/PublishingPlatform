ddf4745c2b513b06536175fdf0db9853d111ab60
i.manzhela <illiamanzhela01@gmail.com>
2023-12-23 14:45:41 +0100

    error.svelte changed

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -2,4 +2,33 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1>{$page.status} {$page.error.message}</h1>
+
+<h1>{$page.status}</h1>
+<h2>{$page.error.message}</h2>
+<div>
+	<a href="/">Go to Main</a>
+	<a href="/login">Go to Login</a>
+</div>
+
+
+<style>
+	h1 {
+		font-size: 4rem;
+		margin: 0;
+		padding-top: 4rem;
+	}
+
+	h1, h2 {
+		text-align: center;
+	}
+
+	div {
+		display: block;
+		text-align: center;
+		font-size: 1.5rem;
+	}
+
+	div a {
+		margin: 1rem;
+	}
+</style>
