78b866c61392a5a1a6aa9ae9e58ce51d9188be6c
i.manzhela <illiamanzhela01@gmail.com>
2023-12-24 19:06:38 +0100

    new layout added with smooth-transitioning sidebar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,33 +4,48 @@
   import { isAuthenticated } from "../stores";
 	import { page } from '$app/stores';
 
-  let isSideMenuVisible = false;
+  let isOpen = false;
  </script>
 
 {#if $page.error !== null}
  <slot />
 {:else}
-  <div class="layout">
-    <Header bind:menuIconArg={isSideMenuVisible}/>
-    <SideMenu bind:isVisible={isSideMenuVisible} isAuthenticated={$isAuthenticated}/>
-  </div>
-  <div class="content" class:cut-width={isSideMenuVisible}>
-    <slot />
+  <div class="main">
+    <div class="home-section">
+      <Header bind:menuIconArg={isOpen}/>
+      <slot />
+    </div>
+    <div class="sidebar" class:open={isOpen} class:close={!isOpen}></div>
   </div>
 {/if}
 
 <style>
-  .layout {
-    height: 6rem;
+  .main { 
     display: flex;
-    flex-direction: row;
-  }
-
-  .content {
     width: 100%;
   }
 
-  .cut-width {
-    width: calc(100% - 42rem);
+  .open {
+    flex: 2;
+  }
+
+  .close {
+    flex: 0;
+  }
+
+  .sidebar {
+    height: 100vh;
+    background-color: #11101d;
+    transition: all 300ms ease-in;
+    z-index: 10;
+    justify-self: flex-end;
+    max-width: 36rem;
+  }
+
+  .home-section {
+    flex: 5;
+    position: relative;
+    height: 100vh;
+    transition: all 300ms ease-in;
   }
 </style>
