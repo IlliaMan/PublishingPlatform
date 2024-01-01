d65fd80b6a562a62651ee5327ba9968eec6084b1
i.manzhela <illiamanzhela01@gmail.com>
2024-01-01 18:39:38 +0100

    Button component added

---
+++ frontend/src/lib/components/Button.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let name = "Button";
+  export let onClick = () => {};
+</script>
+
+<button on:click={onClick}>{name}</button>
+
+<style>
+   button {
+    border-radius: 4px;
+    padding: 0.75rem 3rem;
+    background-color: whitesmoke;
+    border: thin solid var(--primary-color);
+    transition: 50ms;
+  }
+
+  button:hover {
+    cursor: pointer;
+    background-color: var(--secondary-color);
+    color: white;
+  }
+
+  button:active {
+    transform: scale(0.9);
+  }
+</style>
