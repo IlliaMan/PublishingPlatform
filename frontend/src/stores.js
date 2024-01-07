f20e202d50d60025efb39583845559882878f04c
i.manzhela <illiamanzhela01@gmail.com>
2024-01-07 03:55:34 +0100

    sessionStorage is not defined in stores.js fixed

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -1,4 +1,5 @@
 import { writable } from "svelte/store";
+import { browser } from "$app/environment";
 
-export const isAuthenticated = writable(sessionStorage.getItem('jwt') ? true : false);
+export const isAuthenticated = writable(browser && sessionStorage.getItem('jwt') ? true : false);
 export const isAdmin = writable(false);
