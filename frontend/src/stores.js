2af650882b1614473927b06579aba43be7617686
i.manzhela <illiamanzhela01@gmail.com>
2024-01-07 01:46:09 +0100

    logging out when page is refreshed fixed

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -1,4 +1,4 @@
 import { writable } from "svelte/store";
 
-export const isAuthenticated = writable(false);
+export const isAuthenticated = writable(sessionStorage.getItem('jwt') ? true : false);
 export const isAdmin = writable(false);
