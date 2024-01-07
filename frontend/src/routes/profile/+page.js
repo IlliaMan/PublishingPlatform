d8f7511dfcb187b66e68933945d63759fa395209
i.manzhela <illiamanzhela01@gmail.com>
2024-01-07 02:26:13 +0100

    fetching data of /profile page for unauthenticated users added

--- frontend/src/routes/profile/+page.js
+++ frontend/src/routes/profile/+page.js
@@ -1,17 +1,25 @@
 import { error } from '@sveltejs/kit';
+import { isAuthenticated } from '../../stores.js';
+import { get } from 'svelte/store';
 
-export async function load({ fetch }) {
-  const res = await fetch("http://127.0.0.1:3000/articles/user", {
-    headers: {
-      Authorization: `Bearer ${sessionStorage.getItem('jwt')}`
-    }
-  });
+export async function load({ fetch, url }) {
+  let res;
+  console.warn('isAuthenticated', get(isAuthenticated));
+  if(get(isAuthenticated)) {
+    res = await fetch("http://127.0.0.1:3000/articles/user/", {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem('jwt')}`
+      }
+    });
+  } else {
+    const email = url.searchParams.get('email');
+    console.log(email);
+    res = await fetch(`http://127.0.0.1:3000/articles/user/${email}`);
+  }
 
   if(!res.ok) {
     throw error(res.status);
   }
-
-  console.log(res);
     
   const data = await res.json();
   console.log(data);
