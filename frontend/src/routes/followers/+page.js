2e49e88fc04da4014ee0429604052ad20d112d59
i.manzhela <illiamanzhela01@gmail.com>
2024-01-08 12:21:39 +0100

    /followers page added

---
+++ frontend/src/routes/followers/+page.js
@@ -0,0 +1,14 @@
+export async function load({ fetch, url }) {
+  const email = url.searchParams.get('email');
+  const userName = url.searchParams.get('username');
+  let response = await fetch(`http://localhost:3000/users/followers/list/${email}`);
+  if(!response.ok) {
+    throw error(response.status);
+  }
+  response = await response.json();
+  return {
+    followers: response.followers,
+    email,
+    userName
+  };
+}
