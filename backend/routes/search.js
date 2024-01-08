d02658533888d0e2a1d2e32f63e3be268ebd4a01
i.manzhela <illiamanzhela01@gmail.com>
2024-01-08 16:35:01 +0100

    GET /search/:input added

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,9 +1,38 @@
 import express from 'express';
+import ArticleModel from '../models/article.js';
 
 const searchRouter = express.Router();
 
-searchRouter.get('/', async (req, res) => {
-  res.json({ searchResults: ['hello', 'new', 'old']});
+searchRouter.get('/:input', async (req, res) => {
+  let articles;
+  try {
+    articles = await ArticleModel.find({});
+    if(articles == null) {
+      return res.sendStatus(500);
+    }
+  } catch {
+    res.sendStatus(500);
+  }
+
+  let searchResults = articles
+    .filter(article => article.title.toLowerCase().includes(req.params.input.toLowerCase()))
+    .map(article => ({ 
+      id: article._id,
+      title: article.title,
+      email: article.email
+    }));
+
+  if (searchResults.length === 0) {
+    for(let i = 0; i < (articles.length > 7 ? 7 : articles.length); i++) {
+      searchResults.push({ 
+        id: articles[i]._id,
+        title: articles[i].title, 
+        email: articles[i].email
+      });
+    }
+  }
+  
+  res.json({ searchResults });
 });
 
 export default searchRouter;
