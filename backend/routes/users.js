ed01925958d972f7383122080d867ab02ed5129e
i.manzhela <illiamanzhela01@gmail.com>
2024-01-08 02:01:26 +0100

    GET /users/followers/:email/count added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -174,6 +174,21 @@ userRouter.post('/', async (req, res) => {
   }
 });
 
+userRouter.get('/followers/:email/count', async (req, res) => {
+  let user;
+  try {
+    const users = await UserModel.find({ email: req.params.email });
+    if(users == null) {
+      // 404 - cannot find something
+      return res.status(404).json({ message: "cannot find User", users: users });
+    }
+    user = users[0];
+    res.json({ followersCount: user.followers.length });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
 userRouter.get('/following/:email/count', async (req, res) => {
   let user;
   try {
