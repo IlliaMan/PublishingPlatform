e35152aede3893e438b14e042d0edae715f3d767
i.manzhela <illiamanzhela01@gmail.com>
2024-01-14 12:39:42 +0100

    InputReactangle sliding label added

--- frontend/src/lib/components/InputRectangle.svelte
+++ frontend/src/lib/components/InputRectangle.svelte
@@ -1,5 +1,4 @@
 <script>
-  export let name = "Default Name";
   export let type = "text";
   export let placeholder = "placeholder";
   export let inputName = "";
@@ -7,28 +6,57 @@
 </script>
 
 <div>
-  <p>{`${name}:`}</p>
-  <input type={type} placeholder={placeholder} name={inputName} value={value}>
+  <input 
+    type={type} 
+    name={inputName} 
+    placeholder={placeholder} 
+    value={value} 
+  />
+  <label for={inputName}>{placeholder}</label>
 </div>
 
 <style>
 div {
+  position: relative;
   display: flex;
   flex-direction: column;
   padding: 0;
   row-gap: 0.5rem;
 }
 
-p { 
-  align-self: flex-start;
-  margin: 0;
-  font-size: 2rem;
+label {
+  position: absolute;
+  visibility: hidden;
+  background-color: whitesmoke;
+  top: 1.2rem;
+  left: 2rem;
+  font-size: 1.8rem;
+  color: grey;
+  transition: top 200ms ease-in, left 200ms ease-in, font-size 200ms ease-in, visibility 200ms ease-in
+}
+
+input:focus ~ label,
+input:not(:placeholder-shown) ~ label {
+  visibility: visible;
+  box-sizing: border-box;
+  border-radius: 4px;
+  padding: 0.5rem;
+  font-size: 1.6rem;
+  top: -1.6rem;
+  left: 0.8rem;
 }
 
 input {
-  height: 3.5rem;
+  box-sizing: border-box;
+  height: 4.5rem;
   border-radius: 0.5rem;
   font-size: 1.8rem;
   width: 100%;
+  padding-left: 2rem;
+  border: none;
+}
+
+input:focus {
+  outline-color: var(--secondary-color);
 }
 </style>
