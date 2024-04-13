da492d644b40e9349b4da40eedbaf4bcc8e035f5
i.manzhela <illiamanzhela01@gmail.com>
2024-04-13 23:31:50 +0200

    link to /login added to Header

--- frontend-v2/src/components/Header.tsx
+++ frontend-v2/src/components/Header.tsx
@@ -5,11 +5,12 @@ function Header() {
   return (
     <div className="navbar bg-primary text-primary-content">
       <div className="flex-1">
-        <button className="btn btn-ghost text-xl">Publishing Platform</button>
+        <a className="btn btn-ghost text-xl" href="/">Publishing Platform</a>
       </div>
       <div className="navbar-end">
         <button className="btn btn-ghost">Profile</button>
         <button className="btn btn-ghost">Settings</button>
+        <a className="btn btn-ghost" href="/login">Login</a>
       </div>
     </div>
   );
