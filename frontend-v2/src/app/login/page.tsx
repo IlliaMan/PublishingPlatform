edce5052bb4406c8404c8f561363f2486476042d
i.manzhela <illiamanzhela01@gmail.com>
2024-04-13 23:32:29 +0200

    mockup /login page created

---
+++ frontend-v2/src/app/login/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+function LoginPage() {
+  return (
+    <div className="card flex flex-col gap-6 p-8 w-8/12 bg-primary shadow-xl mt-16">
+      <p className="self-center text-xl text-white">LOGIN</p>
+      <p className="self-center text-lg text-white">Welcome to Publish Platfrom</p>
+      <label className="input input-bordered flex items-center gap-2">
+        <input type="text" className="grow" placeholder="Search" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+      </label>
+      <label className="input input-bordered flex items-center gap-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+        <input type="text" className="grow" placeholder="Email" />
+      </label>
+      <div className="flex justify-between">
+        <a className="link text-white">Create Account</a>
+        <a className="link text-white">Forgot Password?</a>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
