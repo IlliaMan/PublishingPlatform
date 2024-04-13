d4b45802741d8de075499d54f2a78d33e947781f
i.manzhela <illiamanzhela01@gmail.com>
2024-04-13 23:10:12 +0200

    app/page mockup implemented

--- frontend-v2/src/app/page.tsx
+++ frontend-v2/src/app/page.tsx
@@ -1,3 +1,20 @@
+import Card from "@/components/Card";
+import Carousel from "@/components/Carousel";
+
 export default function Home() {
-  return <h1>New Frontend for Publishing Platform</h1>;
+  return (
+    <div className="flex flex-col gap-8">
+      <Carousel />
+      <div className="flex flex-row flex-wrap gap-y-8 gap-x-4 justify-between">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 }
